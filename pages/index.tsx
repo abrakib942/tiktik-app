aeb4e590c1033982cb0c20c92b622043271c9bb1
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-02-02 23:21:03 +0600

    sanity added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
 import axios from "axios";
 import { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ videos }) => {
   return <div></div>;
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
 
-  console.log(response.data.name);
+  // console.log(response.data.name);
 
   return {
-    props: {},
+    props: {
+      videos: data,
+    },
   };
 };
 
