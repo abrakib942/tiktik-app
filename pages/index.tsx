b1bc3c3bf19599baad1e416672bd84cadb6251eb
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-02-02 22:09:00 +0600

    sidebar design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div className="text-red-500 font-bold">Hello world</div>;
-}
+import axios from "axios";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div></div>;
+};
+
+export const getServerSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`);
+
+  console.log(response.data.name);
+
+  return {
+    props: {},
+  };
+};
+
+export default Home;
